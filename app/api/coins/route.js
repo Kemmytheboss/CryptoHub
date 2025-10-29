a7174e2610757b80446b097a1dbfd03048cab841
Verah Mokaya <verahmokaya475@gmail.com>
2025-10-30 02:57:45 +0300

    added route js folder to enable the fetch of real data relatime and refreshes every 60seconds

---
+++ app/api/coins/route.js
@@ -0,0 +1,23 @@
+export async function GET() {
+    try {
+        const response = await fetch (
+            'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=5',
+            {
+                headers: {
+                    'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
+                },
+                next: { revalidate: 60 },
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return Response.json(data.data);    
+    } catch (error) {
+        console.error("Error fetching coins:", error);
+        return Response.json({ error: "Failed to fetch coins" }, { status: 500 });
+    }
+}
---
+++ app/api/news.js
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import { title } from "process";
+
+export async function GET() {
+    const apikey = process.env.COINMARKETCAP_API_KEY;
+    const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`;
+    const headers = {
+        'X-CMC_PRO_API_KEY': apikey};
+
+
+    try {
+        const response = await fetch(url, { headers });
+        const data = await response.json();
+        const simplified = data.data.slice(0, 10).map(coin => ({
+            id: coin.id,
+            title: `${coin.name} (${coin.symbol}) update`,
+            body: `{coin.name} is currently priced at $${coin.quote.USD.price.toFixed(2)} with a 24h change of ${coin.quote.USD.percent_change_24h.toFixed(2)}%.`,
+            link: `https://coinmarketcap.com/currencies/${coin.slug}/`,
+        }));
+        return NextResponse.json({ data: simplified });
+    } catch (error) {
+        console.error("Error fetching news:", error);
+        return NextResponse.json({ data: [] }, { status: 500 });
+    }
+}
