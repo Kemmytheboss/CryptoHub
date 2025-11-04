a24b4d0a90804a7223624a7e9c61266f60aeea25
Verah Mokaya <verahmokaya475@gmail.com>
2025-11-04 18:56:40 +0300

    added reference section to update and refer to the Dom in tradingChart component

--- components/TradingChart.jsx
+++ components/TradingChart.jsx
@@ -1,10 +1,18 @@
 'use client';
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import predictions from "../../components/PredictionCard"
+import { useDeprecatedAnimatedState } from "framer-motion";
+
 export default function TradingChart() {
     const container = useRef();
+    const [symbol, setSymbol] = useState("BTCUSDT")
+
+    const coins = ["BTCUSDT", "ETHUSDT", "SOLUSDT", "BNBUSDT", "XRPUSDT", "DOGEUSDT", "TRXUSDT"]
 
     useEffect(() => {
+        if(!container.current) return;
+        container.current.innerHTML = "";
+
         const script = document.createElement("script");
         script.src = "https://s3.tradingview.com/tv.js";
         script.async = true;
@@ -23,11 +31,36 @@ export default function TradingChart() {
             calendar: false,
             support_hosst: "https://tradingview.com",
         });
-        container.current.appendchild(script);
-        return () => container.current.innerHTML= "";
+        container.current.appendChild(script);
     }, [symbol]);
 
     return (
-        <div ref={container} className="tradingview-chart-container" style={{ height: "500px" }}></div>
-    );
+        <section className="p-6 bg-gray-900 text-white rounded-2xl shadow-lg">
+            <h2 className="text-3xl font-bold text-center mb-4">
+                Live Crypto Trading Chart
+            </h2>
+
+            {/* dropdown to select coin */}
+            <div className="flex justify-center mb-6">
+                <select
+                    value={symbol}
+                    onChange={(e)=> {
+                        setSymbol(e.targert.value)
+                    }}
+                    className="bg-gray=800 textwhite px-4 py-2 rounded-md border border-gray">
+
+                        {coins.map((coin)=>value={coin}>
+                            <option key={coin} valuue= {coin}>
+                                {coin.replace("USDT", "")} / <USDT></USDT>
+                            </option>)}
+                    </select>
+            </div>
+
+            {/* CHART CONTAINER */}
+            <div 
+                ref={container}
+                className="tradingview-chart-container rounded-xl overflow-hidden"
+                style={{height:"500px"}}></div>
+        </section>
+        );
 }
