f10dc223ddc041d702714a08dc99adbd1e92b4b2
Verah Mokaya <verahmokaya475@gmail.com>
2025-11-06 20:13:43 +0300

    Ammended the homepage to only have hero section and removed about and contact section

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,10 @@
 import Hero from "./components/Hero";
-import About from "./components/About";
-import Contact from "./components/Contact";
 
  function Home() {
   return (
     <main className="bg-gray-950 text-white">
       <section>
         <Hero/>
-        <About />
-        <Contact />
       </section>
     </main>
   );
